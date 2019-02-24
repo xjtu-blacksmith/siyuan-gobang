76ee086eda91cf071461ca8d59d93e23382ee80c
xjtu-blacksmith <youjiarui189@stu.xjtu.edu.cn>
2019-02-24 11:37:08 +0800

    add some comments in gobang.cpp

--- gobang.cpp
+++ gobang.cpp
@@ -6,39 +6,44 @@ using namespace std;
 bool isin(int,int);
 void showmessage();
 
-class cursor
+// 光标类
+class cursor  
 {
-private:
+private:            // 坐标
 	int x;
 	int y;
 public:
-	cursor(){x=7;y=7;}
-	int getX(){return x;}
+	cursor(){x=7;y=7;}         // 从棋盘中心开始
+    // getter
+	int getX(){return x;}      
 	int getY(){return y;}
+    // 越界判断
 	bool left(){if(x>0)x-=1;else return false;return true;}
 	bool right(){if(x<14)x+=1;else return false;return true;}
 	bool up(){if(y>0)y-=1;else return false;return true;}
 	bool down(){if(y<14)y+=1;else return false;return true;}
+    // setter
 	bool moveto(int tx,int ty){if(isin(tx,ty)){x=tx;y=ty;return true;}else return false;}
 };
 
+// 棋盘类
 class chessboard
 {
 private:
-	int flag[15][15];
-	cursor handle;
+	int flag[15][15];      // 棋盘数组
+	cursor handle;         // 棋盘对应的控制光标
 
-	void f2c(int fg)
+	void f2c(int fg)       // 代号转化为输出字符
 	{
 		switch(fg)
 		{
-			case 0:cout<<"┼";break;
-			case 2:cout<<"○";break;
-			case 3:cout<<"●";break;
+			case 0:cout<<"╋";break;  // 空棋盘
+			case 2:cout<<"○";break;  // 白棋
+			case 3:cout<<"●";break;  // 黑棋
 		}
 	}
 
-	void show(int player)
+	void show(int player)  // 画棋盘，对做GUI参考价值不大
 	{
 		int x=handle.getX();
 		int y=handle.getY();
@@ -74,18 +79,18 @@ private:
 			cout<<"黑方[○]                  [执棋●]白方"<<endl;
 	}
 
-	bool setchessman(int player)
+	bool setchessman(int player)   // 放棋子
 	{
 		int x=handle.getX();
 		int y=handle.getY();
-		if(flag[x][y]!=0)
+		if(flag[x][y]!=0)         // 已有棋子
 			return false;
 		else
-			flag[x][y]=player;
+			flag[x][y]=player;    // 放置当前玩家对应的棋子
 		return true;
 	}
 
-	bool checkrange(int player)
+	bool checkrange(int player)   // 检查是否有玩家获胜
 	{
 		int x=handle.getX();
 		int y=handle.getY();
@@ -103,17 +108,18 @@ private:
 		return false;
 	}
 
+    // 检查每一条线的情况
 	bool checkline(cursor& detector,int player,int para)
 	{
 		int x=detector.getX();
 		int y=detector.getY();
-		int p=1;
+		int p=1;      // 做乘法的基底
 		int q;
 		if(player==2)
-			q=32;
+			q=32;     // 32=2*2*2*2*2
 		else
-			q=243;
-		switch(para)
+			q=243;    // 243=3*3*3*3*3
+		switch(para)  // 确认哪一个方向
 		{
 			case 1:
 				for(int i=-2;i<=2;i++)
@@ -143,7 +149,7 @@ private:
 		return false;
 	}
 
-	int findrange()
+	int findrange()        // 查禁手
 	{
 		int three=0;
 		int four=0;
@@ -153,19 +159,19 @@ private:
 		{
 			if(find_three(para))
 				three++;
-			if(three>=2)
+			if(three>=2)   // 双三禁手（含三四、三三四等）
 				return 3;
 			if(find_four(para))
 				four++;
-			if(four>=2)
+			if(four>=2)    // 双四禁手
 				return 4;
-			if(find_long(para))
+			if(find_long(para))  // 长连禁手
 				return 1;
 		}
-		return 0;
+		return 0;         // 未犯禁手
 	}
 
-	bool find_three(int para)
+	bool find_three(int para)  // 查三三禁手
 	{
 		int x=handle.getX();
 		int y=handle.getY();
@@ -227,7 +233,7 @@ private:
 		}
 	}
 
-	bool find_four(int para)
+	bool find_four(int para)   // 查四四禁手
 	{
 		int x=handle.getX();
 		int y=handle.getY();
@@ -307,7 +313,7 @@ private:
 		}
 	}
 
-	bool find_long(int para)
+	bool find_long(int para)   // 查长连禁手
 	{
 		int x=handle.getX();
 		int y=handle.getY();
@@ -357,14 +363,14 @@ private:
 	}
 
 public:
-	char state[10];
+	char state[10];    // 记录游戏进行状态
 
 	chessboard()
 	{
 		for(int i=0;i<15;i++)
 		for(int j=0;j<15;j++)
-			flag[i][j]=0;
-		strcpy(state,"gaming");
+			flag[i][j]=0;         // 清空棋盘
+		strcpy(state,"gaming");   // 游戏进行中
 	}
 
 	int play(int player)
@@ -372,26 +378,26 @@ public:
 		system("cls");
 		show(player);
 		char behave;
-		behave=getch();
+		behave=getch();           // 获取键盘输入字符
 		switch(behave)
 		{
-			case 72:
+			case 72:              // 左方向键
 				handle.left();
 				break;
-			case 75:
+			case 75:              // 上方向键
 				handle.up();
 				break;
-			case 77:
+			case 77:              // 下方向键
 				handle.down();
 				break;
-			case 80:
+			case 80:              // 右方向键
 				handle.right();
 				break;
-			case 13:
-				if(setchessman(player)){
+			case 13:              // Enter键
+				if(setchessman(player)){      // 尝试放棋子
 					if(player==2)
 					{
-						switch(findrange())
+						switch(findrange())   // 黑方查禁手
 						{
 						case 0:
 							break;
@@ -409,14 +415,14 @@ public:
 							break;
 						}
 					}
-					if(checkrange(player)){
+					if(checkrange(player)){    // 判断输赢
 						if(player==2)
 							cout<<"黑方胜！"<<endl;
 						else if(player==3)
 							cout<<"白方胜！"<<endl;
 						strcpy(state,"win");
 					}
-					player=5-player;
+					player=5-player;   // 对手接盘
 				}
 				break;
 		}
@@ -428,17 +434,16 @@ public:
 
 int main()
 {
-	system("title “思源”杯五子棋竞赛");
-	showmessage();
+	system("title “思源”杯五子棋竞赛");    // 更改窗口标题
+	showmessage();                        // 显示开局信息
 	chessboard game;
-	int counter=2;
+	int counter=2;                        // 黑方先行
 	while(strcmp(game.state,"gaming")==0)
 		counter=game.play(counter);
 	system("pause");
-	
 }
 
-bool isin(int x,int y)
+bool isin(int x,int y)          // 判断越界
 {
 	if(x>=0&&x<15&&y>=0&&y<15)
 		return true;
@@ -447,10 +452,10 @@ bool isin(int x,int y)
 
 void showmessage()
 {
-	cout<<"┏━━━━━━━━━━━━━━━━━┓"<<endl
-		<<"┃ 西安交通大学“思源”杯五子棋竞赛 ┃"<<endl
+	cout<<"┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"<<endl
+		<<"┃  西安交通大学“思源”杯五子棋竞赛  ┃"<<endl
 		<<"┃           竞赛规则宣读           ┃"<<endl
-		<<"┣━━━━━━━━━━━━━━━━━┫"<<endl
+		<<"┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"<<endl
 		<<"┃1. 不得穿拖鞋入场。               ┃"<<endl
 		<<"┃2. 入场前请出示学生证。           ┃"<<endl
 		<<"┃3. 黑方先手，白方后手。用方向键控 ┃"<<endl
@@ -458,7 +463,7 @@ void showmessage()
 		<<"┃4. 查黑方的三三、四四和长连禁手。 ┃"<<endl
 		<<"┃5. 在不违反禁手的前提下，先连五子 ┃"<<endl
 		<<"┃   者获胜，颁发五元加餐卷一张。   ┃"<<endl
-		<<"┗━━━━━━━━━━━━━━━━━┛"<<endl;
+		<<"┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"<<endl;
 	system("pause");
 	system("cls");
 }
